00ecd81bc415a7f0309ce88b14a6ca994e26c456
Alberto Rafael Rodríguez Iglesias <albertocysec@gmail.com>
2018-12-31 20:54:18 +0100

    Añadido algo de html y estructura básica de la API

--- src/main/java/urjc/isi/pruebasSparkJava/Main.java
+++ src/main/java/urjc/isi/pruebasSparkJava/Main.java
@@ -10,12 +10,13 @@ import java.net.URISyntaxException;
 public class Main {
 
 
-    public static String home(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
+    public static String info(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
         String result = new String("TODA LA INFORMACIÓN QUE QUIERAS SOBRE PELÍCULAS");
 
         return result;
 
     }
+
   
     public static String doWork(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
 	String result = new String("Hello World");
@@ -23,12 +24,32 @@ public class Main {
 	return result;
     }
 
+
     public static void main(String[] args) throws ClassNotFoundException {
         port(getHerokuAssignedPort());
 
+	String home = "<html><body>" +
+	"<h1>Bienvenidos a la web de películas</h1>" +
+		"<form action='/info' method='post'>" +
+			"<div class='button'>" +
+				"Ir a info: <br/>" +
+				"<button type='submit'>Información</button>" +
+			"</div>" +
+		"</form></br>"+
+                "<form action='/hello' method='get'>" +
+                        "<div class='button'>" +
+                                "Ir a helloWorld: <br/>" +
+                                "<button type='submit'>Hello</button>" +
+                        "</div>" +
+		"</form>" +
+	"</body></html>";
+
         // spark server
-        get("/", Main::home);
+        get("/", (req, res) -> home);
+        get("/info", Main::info);
         get("/hello", Main::doWork);
+
+	post("/info", Main::info);
 
     }
 
