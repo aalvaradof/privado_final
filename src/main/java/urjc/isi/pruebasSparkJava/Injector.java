9d193bf52aeed56cf7a47c59241f7097f3aefc4f
Carlos Moron Barrios <cmoron@alpha.aulas.gsyc.urjc.es>
2019-01-09 13:46:54 +0100

    Empezando a crear clase inyectora

---
+++ src/main/java/urjc/isi/pruebasSparkJava/Injector.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Injector {
+	
+	private static Connection c;
+	
+	public Injector(String name){
+		try {
+			if(c!=null) return;
+		
+			c = DriverManager.getConnection(name);
+			c.setAutoCommit(false);
+		}catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+	}
+
+	public static String select(String table, String film) {
+		String sql = "SELECT * FROM " + table + " WHERE film=?";
+    	String result = new String();
+    	
+    	try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+    		pstmt.setString(1, film);
+    		ResultSet rs = pstmt.executeQuery();
+    		// Commit after query is executed
+    		c.commit();
+    		while (rs.next()) {
+    			// read the result set
+    			result += "film = " + rs.getString("film") + "\n";
+    			System.out.println("film = "+rs.getString("film") + "\n");
+
+    			result += "actor = " + rs.getString("actor") + "\n";
+    			System.out.println("actor = "+rs.getString("actor")+"\n");
+    		}
+    	} catch (SQLException e) {
+    		System.out.println(e.getMessage());
+    	}
+    	return result;
+	}
+	
+	public void close() {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
+}
